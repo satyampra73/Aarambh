966484f3fb1da2fb0a7f5332351320e2db40f628
Admin <pandeyshraddha2109@gmail.com>
2025-01-15 17:04:50 +0530

    some bugs fixed

--- app/src/main/java/com/example/aarambh/activities/ChangePassword.java
+++ app/src/main/java/com/example/aarambh/activities/ChangePassword.java
@@ -1,6 +1,9 @@
 package com.example.aarambh.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +20,18 @@ public class ChangePassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_change_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Initialize views
+        Button continueButton = findViewById(R.id.continueButton);
+
+        // Set an OnClickListener on the "Continue" button
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChangePassword.this, LoginActivity.class);
+                intent.putExtra("flag","1");
+                startActivity(intent);
+                finish(); // Close the  activity (optional)
+            }
         });
     }
 }
--- app/src/main/java/com/example/aarambh/activities/MobileVerifyActivity.java
+++ app/src/main/java/com/example/aarambh/activities/MobileVerifyActivity.java
@@ -16,7 +16,7 @@ public class MobileVerifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_forgot_password);
+        setContentView(R.layout.activity_mobile_verify);
         // Initialize views
         Button continueButton = findViewById(R.id.continueButton);
 
