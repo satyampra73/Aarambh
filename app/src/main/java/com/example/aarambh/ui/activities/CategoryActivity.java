24d5eef47b91f0d5bf787e50b4e1f2f1dee4747d
Satyam Prajapati <satyam7355pra@gmail.com>
2025-04-02 14:04:57 +0530

    subscribe activity ui created

--- app/src/main/java/com/example/aarambh/ui/activities/CategoryActivity.java
+++ app/src/main/java/com/example/aarambh/ui/activities/CategoryActivity.java
@@ -1,21 +1,30 @@
 package com.example.aarambh.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.aarambh.R;
+import com.example.aarambh.databinding.ActivityCategoryBinding;
 
 public class CategoryActivity extends AppCompatActivity  {
-
-    private RecyclerView recyclerView;
-
+    ActivityCategoryBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
+        binding = DataBindingUtil.setContentView(this , R.layout.activity_category);
 
+        binding.continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CategoryActivity.this,SubscribeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/aarambh/ui/activities/SubscribeActivity.java
+++ app/src/main/java/com/example/aarambh/ui/activities/SubscribeActivity.java
@@ -1,5 +1,6 @@
 package com.example.aarambh.ui.activities;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,15 +8,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.aarambh.R;
+import com.example.aarambh.databinding.ActivitySubscribeBinding;
 
 public class SubscribeActivity extends AppCompatActivity {
-
+    ActivitySubscribeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_subscribe);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_subscribe);
+        binding.txtDiscount1.setPaintFlags(binding.txtDiscount1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        binding.txtDiscount1.setText("₹499");
 
+        binding.txtDiscount2.setPaintFlags(binding.txtDiscount1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        binding.txtDiscount2.setText("₹1499");
+
+        binding.txtDiscount3.setPaintFlags(binding.txtDiscount1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        binding.txtDiscount3.setText("₹2499");
     }
 }
